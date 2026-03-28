b72fb929f1e01390dc40fd19306e799d19e3b6ea
rajath451 <rajathas360@gmail.com>
2026-03-28 16:07:35 +0530

    theme

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useAppContext } from '../context/AppContext'
 
 export const Sidebar = () => {
+
+  const {chats,setSelectedChat,theme,setTheme,user} = useAppContext() //here we can use the context to get the user data and also the chats data to display in the sidebar
+  const [search,setSearch] = useState('')
+
   return (
-    <div>Sidebar</div>
+      <div>
+        {/* Logo */}
+       <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} alt="" /> 
+      </div>
   )
 }
 export default Sidebar
